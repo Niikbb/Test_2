50fe3dd09dd004273628872ad2f3e04636b56557
Niikbb <asd0l@yandex.ru>
2018-03-17 13:22:23 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -83,7 +83,7 @@ var l_bgImageFadeVelocity = 2000;
 
 /*Background darkening amount
 0 for none (0%), 100 for pitch black (100%)*/
-var l_bgDarkening = 25;
+var l_bgDarkening = 10;
 
 /*Music player
 'true' or 'false' to enable music in the background*/
